6de7a3181b1f7d07715ab1f767f5f10628b78aac
ki11errabbit <ki11errabbit@posteo.net>
2023-05-28 19:43:38 -0600

    added documentation to the python files

--- configure.py
+++ configure.py
@@ -6,6 +6,10 @@ from sc_utils import *
 
 
 
+"""
+Creates a dictionary from the a given file name.
+Argument 1 of the script is the file name to use for the dictionary.
+"""
 if __name__ == "__main__":
     create_dictionary(sys.argv[1])
 
--- sc_utils.py
+++ sc_utils.py
@@ -1,13 +1,22 @@
 #!/bin/env python3
 
+"""
+This is a library of helper functions for preparing the dictionary for use by the
+spelling corrector.
+This is a little over-engineered but it is designed to be expanded in the future if needed.
+"""
 
-
-
+"""
+This function takes a filename as input and opens it to be read. It then creates a
+new file called dictionary.rkt and writes each line from the input file into a
+plait define to create a global variable called dictionary which is a list of Strings.
+The main function in main.rkt will use this dictionary to load the spelling corrector.
+"""
 def create_dictionary(dictionary):
     with open(dictionary) as file:
-        string = "(define dictionary (list "
+        string = "#lang plait\n\n(define dictionary (list "
         for word in file:
-            string += word.strip() + " "
+            string += "\"" word.strip() + "\" "
         string += "))"
 
         with open("dictionary.rkt", "w") as output:
